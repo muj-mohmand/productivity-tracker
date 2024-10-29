ece7d1f0ab23808d1434f0d312b25ffa9e2edfef
Muj <muj.mohmand@gmail.com>
2024-10-29 02:26:24 -0400

    Changed to regular function in task page

--- app/task/page.tsx
+++ app/task/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const AddTaskPage = () => {
+export default function AddTaskPage() {
   return (
     <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="pl-4">
@@ -21,6 +21,4 @@ const AddTaskPage = () => {
       </div>
     </div>
   );
-};
-
-export default AddTaskPage;
+}
