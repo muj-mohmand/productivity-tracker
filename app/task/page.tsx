b738ff10bb088ad746d03841e17b81bb652b38cb
Muj <muj.mohmand@gmail.com>
2024-10-29 02:06:06 -0400

    Changed task page function name to AddTaskPage

--- app/task/page.tsx
+++ app/task/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const page = () => {
+export const AddTaskPage = () => {
   return (
     <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="pl-4">
@@ -23,4 +23,4 @@ export const page = () => {
   );
 };
 
-export default page;
+export default AddTaskPage;
