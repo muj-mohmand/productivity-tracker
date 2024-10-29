ba2fdad251744a1b85a65305e19f78ed4e7188fe
Muj <muj.mohmand@gmail.com>
2024-10-28 22:17:41 -0400

    initial commit

---
+++ app/task/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const page = () => {
+  return (
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div className="pl-4">
+        <h2 className="text-lg font-bold">Create New Task</h2>
+        <div className="flow-root">
+          <div className="label">
+            <span className="label-text">What is the task?</span>
+          </div>
+          <div className="flex space-x-2">
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <button className="btn">Add</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["cmyk"],
+  },
 };
 export default config;
